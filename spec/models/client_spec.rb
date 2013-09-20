c5c150f8828fa7b8403fe5a38c61d6ce6674fd91
Doug Tykocki <tykockda@gmail.com>
2013-09-20 04:25:35 -0400

    Added client model specs

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,7 +1,16 @@
 require 'spec_helper'
 
 describe Client do
-  describe "validations" do
+  let(:client) { Client.new(fake_attributes) }
 
+  describe "attributes" do
+    it { expect(client).to validate_presence_of :name   }
+    it { expect(client).to validate_presence_of :email  }
+  end
+
+  private
+
+  def fake_attributes
+    { name: "Person A", email: "example@example.com" }
   end
 end
