a4e9bf74ee9bd23f71c85feee07f66b2232f131b
Doug Tykocki <tykockda@gmail.com>
2013-09-27 04:52:17 -0400

    Add user model and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130922191803) do
+ActiveRecord::Schema.define(version: 20130927083915) do
 
   create_table "clients", force: true do |t|
     t.string   "name"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20130922191803) do
     t.datetime "due_date"
     t.decimal  "amount",     precision: 8, scale: 2
     t.string   "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
