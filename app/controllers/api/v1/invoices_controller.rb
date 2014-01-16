344aef9da31ba36673620cbdf080353263afa0f2
Doug Tykocki <tykockda@gmail.com>
2014-01-16 03:54:11 -0500

    Refactor invoice api controller

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -5,20 +5,17 @@ module Api
       allow_oauth!
 
       def index
-        @client   = current_user.clients.where(params[:client_id]).first
-        @invoices = @client.invoices
+        @invoices = client.invoices
         respond_with @invoices, status: :ok
       end
 
       def show
-        @client   = current_user.clients.where(params[:client_id]).first
-        @invoice  = @client.invoices.where(params[:id]).first
+        @invoice = client.invoices.where(params[:id]).first
         respond_with @invoice, status: :ok
       end
 
       def create
-        @client   = current_user.clients.where(params[:client_id]).first
-        @invoice  = @client.invoices.build(invoice_params)
+        @invoice = client.invoices.build(invoice_params)
         if @invoice.valid?
           @invoice.save
           respond_with @invoice, status: :created, location: api_v1_client_invoices_url(@invoice)
@@ -28,13 +25,19 @@ module Api
       end
 
       def update
-        @client   = current_user.clients.where(params[:client_id]).first
-        @invoice  = @client.invoices.where(params[:id]).first
+        @invoice = client.invoices.where(params[:id]).first
         @invoice.update_attributes(invoice_params)
         respond_with @invoice, status: :ok
       end
 
+      def destroy
+      end
+
       private
+
+      def client
+        @client ||= current_user.clients.where(params[:client_id]).first
+      end
 
       def invoice_params
         params.require(:invoice).permit(:due_date, :amount, :status)
