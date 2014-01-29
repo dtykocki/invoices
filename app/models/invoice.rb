1b529899248427548018308ea54b91bb61f4f589
Doug Tykocki <tykockda@gmail.com>
2014-01-29 10:36:41 -0500

    Require due date for an invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,6 @@
 class Invoice < ActiveRecord::Base
   belongs_to :user
   belongs_to :client
+
+  validates :due_date, presence: true
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,4 +1,7 @@
 require 'spec_helper'
 
 describe Invoice do
+  describe "attributes" do
+    it { should validate_presence_of(:due_date) }
+  end
 end
